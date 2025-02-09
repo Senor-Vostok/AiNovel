ec846b8b2f177d492e056192c1de2efa21d649ef
Senor_Vostok <tabulgatin1@gmail.com>
2025-02-10 00:53:25 +0500

    Фоны для заднего использования

--- Engine/Objects/UI/Interfaces.py
+++ Engine/Objects/UI/Interfaces.py
@@ -3,7 +3,7 @@ from Engine.Objects.UI.Widgets import *
 
 class Test:  # Специально для Богданчика пример с интерфейсом
     def __init__(self, language_data, xoy, textures):
-        xoy = (xoy[0], xoy[1] + 100)
-        self.hello = Label("Hello Team 1 :))", xoy, 80, (48, 35, 22))
-        self.image = Image(textures.characters["character19"][0], (xoy[0], xoy[1] - 200))
-        self.surface = Surface(self.image, self.hello)
+        self.hello = Label("Hello Team 1 :))", (xoy[0], xoy[1] + 100), 80, (48, 35, 22))
+        self.image = Image(textures.characters["character19"][0], (xoy[0], xoy[1] - 100))
+        self.background = Image(textures.locations["NightStripteaseClub"][0], xoy)
+        self.surface = Surface(self.background, self.image, self.hello)
