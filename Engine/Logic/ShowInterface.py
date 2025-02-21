eee9b3c8a6b3dedeb6a2575a1c86efa97bb4b107
Napordisch <bogdankhamidullin@gmail.com>
2025-02-21 19:35:02 +0500

    unfinished new story screen and some refactorings

--- Engine/Logic/ShowInterface.py
+++ Engine/Logic/ShowInterface.py
@@ -1,6 +1,19 @@
 from Engine.Objects.UI import Interfaces
+from Engine.Objects.UI import Widgets
 
+def show_UI(handler, centre):
+    # handler.interfaces['test'] = Interfaces.Test(None, centre, size, handler.textures)
+    Entry = Widgets.Entry
+    # handler.interfaces['main_menu'] = Interfaces.MainMenu(None, centre, handler.textures,
+    #                                                       entries=[Entry("story 1"),
+    #                                                             Entry("story 2"),
+    #                                                             Entry("story 3"),
+    #                                                             Entry("Story 4"),
+    #                                                             Entry("Story 5"),
+    #                                                             Entry("Story 6"),
+    #                                                             Entry("Story 7"),
+    #                                                             Entry("Story 8"),
+    #                                                             Entry("Story 9"),
+    #                                                             Entry("Story 10")])
 
-def show_UI(self, centre):
-    # self.interfaces['test'] = Interfaces.Test(None, centre, size, self.textures)
-    self.interfaces['main_menu'] = Interfaces.MainMenu(None, centre, self.textures)
+    handler.interfaces['new_story_creation_screen'] = Interfaces.NewStoryCreationScreen(None, centre, handler.textures)
--- Engine/Objects/UI/Interfaces.py
+++ Engine/Objects/UI/Interfaces.py
@@ -1,54 +1,63 @@
-from Engine.Objects.UI.Widgets import *
+# from Engine.Objects.UI.Widgets import *
+from Engine.Objects.UI import Widgets as W
 import random
+screen_width = None
+screen_height = None
 
+def set_screen_size(xoy):
+    global screen_width
+    global screen_height
+    screen_width = xoy[0] * 2
+    screen_height = xoy[1] * 2
 
 class Test:
     def __init__(self, language_data, xoy, textures):
-        self.hello = Label("Hello Team 1 :))", (xoy[0], xoy[1] + 100), 80, (255, 255, 255))
+        self.hello = W.Label("Hello Team 1 :))", (xoy[0], xoy[1] + 100), 80, (255, 255, 255))
 
-        self.image = Image(textures.characters["character19"][0], (xoy[0], xoy[1] - 200))
+        self.image = W.Image(textures.characters["character19"][0], (xoy[0], xoy[1] - 200))
 
-        self.figure = Figure([xoy[0], xoy[1] - 100], (48, 35, 22, 255),
+        self.figure = W.Figure([xoy[0], xoy[1] - 100], (48, 35, 22, 255),
                              form=[[100, 30], [30, 100], [100, 100], [30, 30], [100, 30]], thickness=2,
                              color_bord=(252, 0, 0, 255))
 
-        self.circle = Circle([xoy[0], xoy[1] + 200], (48, 35, 22, 255), radius=100, thickness=2,
+        self.circle = W.Circle([xoy[0], xoy[1] + 200], (48, 35, 22, 255), radius=100, thickness=2,
                              color_bord=(252, 0, 0, 0))
 
-        self.dropdown = DropDown(textures.DropDown["selected"] + textures.DropDown["arrow"] + textures.DropDown["variant"], (xoy[0], xoy[1] + 200), ("1", "2", "3", "4", "5"))
-        self.background = Image(textures.locations["InsideTheCircusTent"][0], xoy)
-        self.surface = Surface(self.background, self.image, self.hello, self.figure, self.dropdown)
-
-
-class Entry:
-    def __init__(self, text: str, function=None):
-        self.text = text
-        self.function = function
+        self.dropdown = W.DropDown(textures.DropDown["selected"] + textures.DropDown["arrow"] + textures.DropDown["variant"], (xoy[0], xoy[1] + 200), ("1", "2", "3", "4", "5"))
+        self.background = W.Image(textures.locations["InsideTheCircusTent"][0], xoy)
+        self.surface = W.Surface(self.background, self.image, self.hello, self.figure, self.dropdown)
 
 
 class MainMenu:
-    def __init__(self, language_data, xoy, textures):
+    def __init__(self, language_data, xoy, textures, entries):
+        global screen_width
+        global screen_height
+
+        set_screen_size(xoy)
 
         #example entries
-        self.entries = [Entry("story 1"),
-                        Entry("story 2"),
-                        Entry("story 3"),
-                        Entry("Story 4"),
-                        Entry("Story 5"),
-                        Entry("Story 6"),
-                        Entry("Story 7"),
-                        Entry("Story 8"),
-                        Entry("Story 9"),
-                        Entry("Story 10"),]
-        # self.entries = [Entry("story 1"),
-        #                 Entry("story 2"),
-        #                 Entry("story 3")]
+        if entries is not None:
+            self.entries = entries
+        else:
+            self.entries = [W.Entry("story 1"),
+                            W.Entry("story 2"),
+                            W.Entry("story 3"),
+                            W.Entry("Story 4"),
+                            W.Entry("Story 5"),
+                            W.Entry("Story 6"),
+                            W.Entry("Story 7"),
+                            W.Entry("Story 8"),
+                            W.Entry("Story 9"),
+                            W.Entry("Story 10")]
+            # handler.entries = [W.Entry("story 1"),
+            #                 W.Entry("story 2"),
+            #                 W.Entry("story 3")]
 
-        self.surface = Surface()
+        self.surface = W.Surface()
         self.base_widget_kit = []
-        self.screen_height = xoy[1] * 2
-        self.screen_width = xoy[0] * 2
-        print(f"Screen Dimensions: {self.screen_width}×{self.screen_height}")
+
+
+        print(f"Screen Dimensions: {screen_width}×{screen_height}")
 
         # цвета будут определяться с помощью цвета фона, фоны при наведении — меняться, поэтому для каждой кнопки свой цвет обводки
         self.button_state_color = (48, 35, 22, 0)
@@ -63,7 +72,7 @@ class MainMenu:
         cell_height = self.cell_height()
         print("Cell height: ", cell_height)
         vertical_postiion = 0 + cell_height * 2
-        horizontal_position = self.screen_width // 2
+        horizontal_position = screen_width // 2
         for entry in self.entries:
             print(vertical_postiion)
 
@@ -73,7 +82,7 @@ class MainMenu:
                                               self.button_trigger_color,
                                               max_height=cell_height * 2)
 
-            entry_button = Button(xoy=(horizontal_position, vertical_postiion),
+            entry_button = W.Button(xoy=(horizontal_position, vertical_postiion),
                                   images=[button_state_figure.surface, button_trigger_figure.surface],
                                   text=entry.text)
 
@@ -85,34 +94,90 @@ class MainMenu:
 
 
     def cell_height(self) -> int:
+        global screen_height
+        global screen_width
         amount_of_entries = len(self.entries)
         amount_of_cells = amount_of_entries * 2 + amount_of_entries + 1
-        return self.screen_height // amount_of_cells
+        return screen_height // amount_of_cells
 
 
-    def create_button_figures(self, position, button_text: str, color: tuple[int, int, int, int], max_height: int) -> tuple[Figure, Figure]:
+    def create_button_figures(self, position, button_text: str, color: tuple[int, int, int, int], max_height) -> tuple[W.Figure, W.Figure]:
 
         if max_height is None:
             max_height = self.cell_height()
 
         max_vertical_distance = max_height // 2
-        horizontal_safe_zone_size = int(self.screen_width - (0.1 * self.screen_width)) // 2
+        horizontal_safe_zone_size = int(screen_width - (0.1 * screen_width)) // 2
         max_horizontal_distance = min(max_height * len(button_text), horizontal_safe_zone_size)
 
         vertical_distance = lambda: random.randint(int(max_vertical_distance - (0.1 * max_vertical_distance)),
                                                    max_vertical_distance)
         horizontal_distance = lambda: random.randint(int(max_horizontal_distance - (0.1 * max_horizontal_distance)),
                                                      max_horizontal_distance)
+
         bottom_left = [-horizontal_distance(), -vertical_distance()]
         top_left = [-horizontal_distance(), vertical_distance()]
         top_right = [horizontal_distance(), vertical_distance()]
         bottom_right = [horizontal_distance(), -vertical_distance()]
 
-        return Figure(xoy=position,
+        return W.Figure(xoy=position,
                       color=color,
                       form=[top_left, top_right, bottom_right, bottom_left],
                       thickness=2), \
-               Figure(xoy=position,
+               W.Figure(xoy=position,
                       color=(0, 0, 0, 0,),
                       form=[top_left, top_right, bottom_right, bottom_left],
                       thickness=2)
+
+class NewStoryCreationScreen:
+    def __init__(self, language_data, xoy, textures):
+        global screen_height
+        global screen_width
+        set_screen_size(xoy)
+        print(screen_width)
+        print(xoy[0])
+        left_column_horizontal_position = screen_width * 0.1
+        print(left_column_horizontal_position)
+
+        self.title = W.Label(text="Новая история",
+                             xoy=(left_column_horizontal_position, screen_height * 0.1),
+                             pp=80, color=(0, 0, 0), centric=False)
+
+        self.genre_label = W.Label(text="Жанр",
+                                   xoy=(left_column_horizontal_position, screen_height * 0.2),
+                                   pp=80, color=(0, 0, 0), centric=False)
+
+        self.actors_label = W.Label(text="Действующие лица",
+                                    xoy=(left_column_horizontal_position, screen_height * 0.3),
+                                    pp=80, color=(0, 0, 0), centric=False)
+
+        self.desctiption_label = W.Label(text="Описание или вступление:",
+                                         xoy=(left_column_horizontal_position, screen_height * 0.45),
+                                         pp=80, color=(0, 0, 0), centric=False)
+
+
+        description_field_position = (xoy[0], screen_height * 0.7)
+        description_field_height = screen_height * 0.3
+
+        top_left = (-(xoy[0] - left_column_horizontal_position),
+                    description_field_height // 2)
+
+        top_right = ((xoy[0] - left_column_horizontal_position),
+                     description_field_height // 2)
+
+        bottom_right = ((xoy[0] - left_column_horizontal_position),
+                        -(description_field_height // 2))
+
+        bottom_left = (-(xoy[0] - left_column_horizontal_position),
+                       -(description_field_height // 2))
+
+
+        self.description_field_figure = W.Figure(xoy=description_field_position,
+                                                 color=(255,255,255),
+                                                 color_bord=(0, 0, 0, 255),
+                                                 form=[top_left, top_right, bottom_right, bottom_left],
+                                                 thickness=2)
+
+        self.elements = [self.title, self.genre_label, self.actors_label, self.desctiption_label, self.description_field_figure]
+
+        self.surface = W.Surface(*self.elements)
