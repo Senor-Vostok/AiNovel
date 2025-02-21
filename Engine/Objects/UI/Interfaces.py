058847740badae18c047e3cc236a1f47c0d5438f
Napordisch <bogdankhamidullin@gmail.com>
2025-02-21 15:47:58 +0500

    Formatting

--- Engine/Objects/UI/Interfaces.py
+++ Engine/Objects/UI/Interfaces.py
@@ -5,12 +5,16 @@ import random
 class Test:
     def __init__(self, language_data, xoy, textures):
         self.hello = Label("Hello Team 1 :))", (xoy[0], xoy[1] + 100), 80, (255, 255, 255))
+
         self.image = Image(textures.characters["character19"][0], (xoy[0], xoy[1] - 200))
+
         self.figure = Figure([xoy[0], xoy[1] - 100], (48, 35, 22, 255),
                              form=[[100, 30], [30, 100], [100, 100], [30, 30], [100, 30]], thickness=2,
                              color_bord=(252, 0, 0, 255))
+
         self.circle = Circle([xoy[0], xoy[1] + 200], (48, 35, 22, 255), radius=100, thickness=2,
                              color_bord=(252, 0, 0, 0))
+
         self.dropdown = DropDown(textures.DropDown["selected"] + textures.DropDown["arrow"] + textures.DropDown["variant"], (xoy[0], xoy[1] + 200), ("1", "2", "3", "4", "5"))
         self.background = Image(textures.locations["InsideTheCircusTent"][0], xoy)
         self.surface = Surface(self.background, self.image, self.hello, self.figure, self.dropdown)
@@ -52,33 +56,49 @@ class MainMenu:
 
         self.update_displayed_entries()
 
+
     def update_displayed_entries(self):
         print("amount of entries: ", len(self.entries))
         self.surface.widgets = self.base_widget_kit
         cell_height = self.cell_height()
         print("Cell height: ", cell_height)
-        vertical_postiion = 0  + cell_height * 2
+        vertical_postiion = 0 + cell_height * 2
         horizontal_position = self.screen_width // 2
         for entry in self.entries:
             print(vertical_postiion)
-            button_trigger_figure, button_state_figure = self.create_button_figures((horizontal_position, vertical_postiion), entry.text, self.button_trigger_color, max_height=cell_height * 2)
-            entry_button = Button(xoy=(horizontal_position, vertical_postiion), images=[button_state_figure.surface, button_trigger_figure.surface], text=entry.text)
+
+            button_trigger_figure, button_state_figure = \
+                  self.create_button_figures((horizontal_position, vertical_postiion),
+                                              entry.text,
+                                              self.button_trigger_color,
+                                              max_height=cell_height * 2)
+
+            entry_button = Button(xoy=(horizontal_position, vertical_postiion),
+                                  images=[button_state_figure.surface, button_trigger_figure.surface],
+                                  text=entry.text)
+
             if entry.function is not None:
                 entry_button.connect(entry.function)
+
             self.surface.widgets.append(entry_button)
             vertical_postiion += cell_height * 3
+
 
     def cell_height(self) -> int:
         amount_of_entries = len(self.entries)
         amount_of_cells = amount_of_entries * 2 + amount_of_entries + 1
         return self.screen_height // amount_of_cells
 
+
     def create_button_figures(self, position, button_text: str, color: tuple[int, int, int, int], max_height: int) -> tuple[Figure, Figure]:
+
         if max_height is None:
             max_height = self.cell_height()
+
         max_vertical_distance = max_height // 2
         horizontal_safe_zone_size = int(self.screen_width - (0.1 * self.screen_width)) // 2
         max_horizontal_distance = min(max_height * len(button_text), horizontal_safe_zone_size)
+
         vertical_distance = lambda: random.randint(int(max_vertical_distance - (0.1 * max_vertical_distance)),
                                                    max_vertical_distance)
         horizontal_distance = lambda: random.randint(int(max_horizontal_distance - (0.1 * max_horizontal_distance)),
@@ -87,11 +107,12 @@ class MainMenu:
         top_left = [-horizontal_distance(), vertical_distance()]
         top_right = [horizontal_distance(), vertical_distance()]
         bottom_right = [horizontal_distance(), -vertical_distance()]
+
         return Figure(xoy=position,
                       color=color,
                       form=[top_left, top_right, bottom_right, bottom_left],
                       thickness=2), \
-            Figure(xoy=position,
-                   color=(0, 0, 0, 0,),
-                   form=[top_left, top_right, bottom_right, bottom_left],
-                   thickness=2)
+               Figure(xoy=position,
+                      color=(0, 0, 0, 0,),
+                      form=[top_left, top_right, bottom_right, bottom_left],
+                      thickness=2)
