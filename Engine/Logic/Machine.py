93b905af717a42b49bc6238d8990f0d6b84c282e
MaksimJfg <84992474+MaksimJfg@users.noreply.github.com>
2025-02-24 21:15:42 +0500

    Delete Engine directory

--- Engine/Logic/Machine.py
+++
@@ -1,6 +0,0 @@
-class Scene:
-    def __init__(self, handler):
-        self.handler = handler
-        self.textures = handler.textures
-        self.synchronous = 1
-        self.rendering = False
--- Engine/Logic/ShowInterface.py
+++
@@ -1,6 +0,0 @@
-from Engine.Objects.UI import Interfaces
-from Engine.Objects.UI import Widgets
-
-
-def show_UI(handler, centre):
-    handler.interfaces['main_menu'] = Interfaces.MainMenu(None, centre, handler.textures, handler.saves_story)
