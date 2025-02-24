c46e1bed9278eb2ad284a972cf486b3c62acabd3
MaksimJfg <84992474+MaksimJfg@users.noreply.github.com>
2025-02-24 21:16:21 +0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from Engine.Launch.Handler import EventHandler
-import pygame
-
-if __name__ == "__main__":
-    pygame.init()
-    handler = EventHandler()
-    while True:
-        handler.update()
