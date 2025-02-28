4123c49fb0b0fc6cc77cf5fed8675fc53ae09a1f
temich91 <temich-ivanoff@yandex.ru>
2025-02-28 13:12:41 +0500

    Загрузка ассетов в хендлере, сообщение о существующем файле

--- Engine/Launch/Handler.py
+++ Engine/Launch/Handler.py
@@ -1,5 +1,9 @@
 import copy
 import sys
+import os
+import shutil
+import tkinter as tk
+from tkinter import filedialog, messagebox
 import pygame.display
 from Engine.Texture.Textures import Textures
 from Engine.Sound.Sounds import Sounds
@@ -21,7 +25,7 @@ class EventHandler:
         # handler.language_data = dict()
         # handler.export_language(handler.settings['language'])
         self.size = GetSystemMetrics(0), GetSystemMetrics(1)
-        self.centre = (self.size[0] // 2, self.size[1] // 2)
+        self.center = (self.size[0] // 2, self.size[1] // 2)
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode(self.size, pygame.DOUBLEBUF | pygame.HWSURFACE | pygame.RESIZABLE)
         self.textures = Textures()
@@ -42,7 +46,7 @@ class EventHandler:
                             {"name": "История бобра Максима", "launch": None},
                             {"name": "История бобра Максима", "launch": None},
                             {"name": "История бобра Максима", "launch": None}]
-        show_UI(self, self.centre)
+        show_UI(self, self.center)
         # handler.__xoy_information = [handler.centre[0] * 2, handler.centre[1] * 2 - handler.textures.land['barrier'][0].get_rect()[2]]
 
     # def change_volume(handler, slicer, channel):
@@ -50,11 +54,11 @@ class EventHandler:
     #     pygame.mixer.Channel(channel).set_volume(handler.volumes_channels[channel])
 
     def click_handler(self):
-        c = None
+        command = None
         for i in pygame.event.get():
             self.camera.event(i)
             if i.type == pygame.KEYDOWN:
-                c = i
+                command = i
                 if i.key == pygame.K_ESCAPE:
                     if len(self.interfaces) > 1:
                         self.interfaces.pop([_ for _ in self.interfaces if self.interfaces[_] == self.last_interface][0])
@@ -65,12 +69,41 @@ class EventHandler:
                 if 'choicegame' in self.interfaces: self.interfaces.pop('choicegame')
             if i.type == pygame.QUIT:
                 self.quit()
-        return c
+        return command
 
     def update(self):
         self.screen.fill(BACKGROUND_COLOR)
         self.clock.tick()
         self.render.rendering()
+
+    def get_new_asset_name(self):
+        asset_number = len(os.listdir("Assets/Characters")) + 1
+        return f"Characters/character{asset_number}.png"
+
+    def choose_asset(self, asset_type):
+        root = tk.Tk()
+        root.withdraw()
+
+        file_path = tk.filedialog.askopenfilename(
+            initialdir="/",
+            title="Выберите файл",
+            filetypes=[("PNG files", "*.png")])
+
+        if file_path:
+            if asset_type == "character":
+                file_name = self.get_new_asset_name()
+            elif asset_type == "location":
+                file_name = "Locations/" + os.path.basename(file_path)
+                if os.path.exists(f"Assets/{file_name}"):
+                    messagebox.showerror("Добавление файла",
+                                         f"Файл с названием {os.path.basename(file_path)} уже добавлен.")
+                    return
+            else:
+                return
+
+            shutil.copy(file_path, f"Assets/{file_name}")
+        else:
+            return
 
     #def quit(self):
     #     sys.exit()
